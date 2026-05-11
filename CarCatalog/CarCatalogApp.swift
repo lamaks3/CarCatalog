325173eb4b8d46682ec6007edf0edadd96d7e381
Maksim Shyshko <lamakswork@gmail.com>
2026-05-11 10:56:56 +0300

    Initial commit

---
+++ CarCatalog/CarCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCatalogApp.swift
+//  CarCatalog
+//
+//  Created by Maksim Shyshko on 11.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct CarCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarCatalogTests/CarCatalogTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarCatalogTests.swift
+//  CarCatalogTests
+//
+//  Created by Maksim Shyshko on 11.05.2026.
+//
+
+import Testing
+@testable import CarCatalog
+
+struct CarCatalogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
