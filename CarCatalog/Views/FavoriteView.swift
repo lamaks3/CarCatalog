e1c7428bff6386f5ca3e3e3dea201dbf59e171ec
Maksim Shyshko <lamakswork@gmail.com>
2026-05-26 20:19:40 +0300

    Improve Favorite Cars view design

--- CarCatalog/Views/FavoriteView.swift
+++ CarCatalog/Views/FavoriteView.swift
@@ -10,16 +10,45 @@ import SwiftUI
 struct FavoriteView: View {
     @ObservedObject var carStore: CarStore
     var body: some View {
-        var favorites = carStore.favorites
-        List {
-            if favorites.isEmpty {
-                Text("No favorite cars added")
-            } else {
-                ForEach(favorites) { car in
-                    Text(car.model)
+        let favorites = carStore.favorites
+        VStack(spacing: 0) {
+            FavoritesHeader()
+            List {
+                if favorites.isEmpty {
+                    Text("No favorite cars added")
+                } else {
+                    ForEach(favorites) { car in
+                        CarInfo(car: car)
+                    }
                 }
             }
         }
+    }
+}
+
+struct FavoritesHeader: View {
+    var body: some View {
+        HStack {
+            Text("Favorite Cars")
+                .font(Font.largeTitle.bold())
+
+            Spacer()
+        }
+        .padding()
+        .background(
+            ZStack {
+                Color.yellow.opacity(0.1)
+                HStack {
+                    Spacer()
+                    Image(systemName: "star.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .opacity(0.1)
+                }
+            }
+
+            .ignoresSafeArea(edges: .top)
+        )
     }
 }
 
