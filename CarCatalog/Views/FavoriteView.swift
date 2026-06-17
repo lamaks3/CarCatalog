3f19b1787d8c0e8704fe6bf8b01aa6c645d73b1c
Maksim Shyshko <lamakswork@gmail.com>
2026-06-17 16:04:41 +0300

    Add interaction with elements in favorite list

--- CarCatalog/Views/FavoriteView.swift
+++ CarCatalog/Views/FavoriteView.swift
@@ -18,7 +18,19 @@ struct FavoriteView: View {
                     Text("No favorite cars added")
                 } else {
                     ForEach(favorites) { car in
-                        CarInfo(car: car)
+                        NavigationLink {
+                            CarDetailView(carStore: carStore, car: car)
+                        } label: {
+                            CarInfo(car: car)
+                            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                                Button(role: .destructive) {
+                                    carStore.toggleFavorite(car)
+                                } label: {
+                                    Label("Remove from Favorites", systemImage: "star.slash.fill")
+                                }
+                                .tint(.yellow)
+                            }
+                        }
                     }
                 }
             }
